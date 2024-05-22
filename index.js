6f5296b02920fbb5e649f32eeee48c5a196a88a3
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2024-05-22 20:20:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8000;
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://movies-tickets.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 server.use(middlewares);
 server.use(router);
 
