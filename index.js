866a25a4fa7d4a066525fcbad0b64aadeb49cc54
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2024-05-22 20:54:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const port = process.env.PORT || 8000;
 import cors from 'cors';
 
 // Cors
-server.use(middlewares);
-const corsOptions = {
-  origin: "https://movies-tickets.onrender.com",
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-server.use(cors(corsOptions));
+// server.use(middlewares);
+// const corsOptions = {
+//   origin: "https://movies-tickets.onrender.com",
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
+// server.use(cors(corsOptions));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 server.use(router);
-
 server.listen(port);
