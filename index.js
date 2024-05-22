b8bdfe68e27384dd97f7298530db892f87c2436c
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2024-05-22 21:12:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,8 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8000;
+import cors from "cors"
 
-
-server.use(middlewares);
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://movies-tickets.onrender.com')
-  res.header('Access-Control-Allow-Headers', '*')
-  next()
-})
+server.use(cors())
 server.use(router);
 server.listen(port);
