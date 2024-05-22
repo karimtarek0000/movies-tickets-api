21741d5f727834caa9e64a7e808aa6bcf1cf3b6a
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2024-05-22 21:05:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8000;
 
 
+server.use(middlewares);
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'https://movies-tickets.onrender.com')
   res.header('Access-Control-Allow-Headers', '*')
   next()
 })
